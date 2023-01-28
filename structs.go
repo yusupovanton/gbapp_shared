381c9bff19eecf396eda72e4f5c4f44c03b12c7c
yusupovanton <antonyusupov@outlook.com>
2023-01-28 15:30:13 +0300

    structs modified for use with redis - moving to v0.1.0

--- structs.go
+++ structs.go
@@ -11,9 +11,9 @@ type User struct {
 	gorm.Model
 	Ads           pq.Int64Array `json:"ads" gorm:"type:integer[]"`
 	CurrentAd     uint          `json:"currentad,omitempty"`
-	Username      string        `json:"username"`
-	StateName     string        `json:"statename,omitempty"`
-	StateOverride bool          `json:"stateoverride,omitempty"`
+	Username      string        `json:"username" redis:"username"`
+	StateName     string        `json:"statename,omitempty" redis:"statename"`
+	StateOverride bool          `json:"stateoverride,omitempty" redis:"stateoverride"`
 	Password      string        `json:"password"`
 }
 
@@ -27,14 +27,14 @@ type User struct {
 
 type Ad struct {
 	gorm.Model
-	User_id     uint
-	Username    string
-	Price       string
-	Category    string
-	Title       string
-	Location    string
-	Contacts    string
-	Description string
+	User_id     uint   `redis:"user_id"`
+	Username    string `redis:"username"`
+	Price       string `redis:"price"`
+	Category    string `redis:"category"`
+	Title       string `redis:"title"`
+	Location    string `redis:"location"`
+	Contacts    string `redis:"contacts"`
+	Description string `redis:"description"`
 	Post        bool
 	Last_posted time.Time
 	Images      pq.StringArray `gorm:"type:string[]"`
